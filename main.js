4aa93a839e63fe956a772a4c45ffa0bbc0579ad1
Auh3b <ngaiyayec@yahoo.com>
2020-08-16 20:19:55 +0200

    added js

---
+++ main.js
@@ -0,0 +1,33 @@
+// // menu background shadow
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY > 300)   {
+//         document.querySelector('#navbar').style.box-shadow = 100 0 20 #333;
+//     }   else {
+//         document.querySelector('#navbar').style.opacity = 1
+//     };
+// });
+
+// // sticky menu background
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY > 150)   {
+//         document.querySelector('#navbar').style.opacity = 0.9;
+//     }   else {
+//         document.querySelector('#navbar').style.opacity = 1
+//     };
+// });
+
+// smooth scrolling
+$("#navbar  a,  .btn").on("click", function (event) {
+  if (this.hash !== " ") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html,    body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
